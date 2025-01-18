9eb7ba31eb8b02902436785e8852a9e74814e8f0
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-01-18 14:45:06 -0700

    fix

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,6 +2,6 @@ import type { Address } from 'viem';
 import * as CONTRACT_ABI from '../../../contracts/artifacts-zk/contracts/Brickles.sol/Brickles.json';
 
 export const ABI = CONTRACT_ABI.abi;
-export const GAME_CONTRACT_ADDRESS: Address = '0x...';
-export const PAYMASTER_CONTRACT_ADDRESS: Address = '0x...';
+export const GAME_CONTRACT_ADDRESS: Address = import.meta.env.VITE_GAME_CONTRACT_ADDRESS ?? '0x...';
+export const PAYMASTER_CONTRACT_ADDRESS: Address = import.meta.env.VITE_PAYMASTER_CONTRACT_ADDRESS ?? '0x...';
 export const API_URL = 'http://localhost:8000';
